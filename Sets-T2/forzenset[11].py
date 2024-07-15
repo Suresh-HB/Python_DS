6f58ee94f96d33345299c07858e2f72644f9ba05
Suresh HB <sureshsai175475@gmail.com>
2024-07-15 20:19:03 +0530

    [Suresh] Add : Python Program to use of frozensets

---
+++ Sets-T2/forzenset[11].py
@@ -0,0 +1,21 @@
+'''
+@Author: Suresh
+@Date: 2024-07-12
+@Last Modified by: Suresh
+@Last Modified: 2024-07-12 
+@Title : Program to use of frozensets
+'''
+
+
+def frozenset():
+    animals = frozenset(["cat", "dog", "lion"])
+    print("cat" in animals) 
+    print("elephant" in animals) 
+
+
+def main():
+    frozenset()
+
+
+if __name__ == '__main__':
+   main()
