95c12602720a8bde023f68b1862e9d15393975c6
Suresh HB <sureshsai175475@gmail.com>
2024-07-15 20:28:56 +0530

    [Suresh] Add : Python program to unpacking the elements from tuple

---
+++ Tuple-T2/unPackingTuple[3].py
@@ -0,0 +1,19 @@
+'''
+@Author: Suresh
+@Date: 2024-07-13
+@Last Modified by: Suresh
+@Last Modified: 2024-07-13 
+@Title : Python program to unpacking the elements from tuple.
+'''
+
+
+def main():
+    my_tuple = (1, 2, 3)
+    a, b, c = my_tuple
+    print("a =", a)
+    print("b =", b)
+    print("c =", c)
+
+
+if __name__ =='__main__':
+    main()
