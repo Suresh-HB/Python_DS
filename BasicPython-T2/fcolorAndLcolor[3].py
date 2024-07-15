8cb8ef70b1c2ec0ff1122a6ab23577ad015e221e
Suresh HB <sureshsai175475@gmail.com>
2024-07-15 20:55:43 +0530

    [Suresh] Add : Python program for frist and last color

---
+++ BasicPython-T2/fcolorAndLcolor[3].py
@@ -0,0 +1,20 @@
+'''
+@Author: Suresh
+@Date: 2024-07-10 
+@Last Modified by: Suresh
+@Last Modified: 2024-07-11 
+@Title : Python program for frist and last color
+'''
+
+def fristAndLastColor():
+    color_list = ["Red", "Green", "White", "Black"]
+    first_color = color_list[0]
+    print("First color:", first_color)
+    last_color = color_list[-1]
+    print("Last color:", last_color)
+
+def main():
+    fristAndLastColor()
+
+if __name__ == '__main__':
+    main()
