bcd10fbbe7677c071730a09ceeee09b2b0813ea4
Suresh HB <sureshsai175475@gmail.com>
2024-07-15 20:27:37 +0530

    [Suresh] Add : Python program to creating the tuple

---
+++ Tuple-T2/creating_tuple[1].py
@@ -0,0 +1,17 @@
+'''
+@Author: Suresh
+@Date: 2024-07-13
+@Last Modified by: Suresh
+@Last Modified: 2024-07-13 
+@Title : Python program to creating the tuple.
+'''
+
+
+def main():
+    my_tuple = (1, 2, 3, 4, 5)
+    print("Tuple:", my_tuple)
+
+
+if __name__ == '__main__':
+    main()
+
