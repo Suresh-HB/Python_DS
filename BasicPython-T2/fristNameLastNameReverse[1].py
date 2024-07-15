e48e7b6149f1bff5bcee9aa393016ba7f5ec1cf3
Suresh HB <sureshsai175475@gmail.com>
2024-07-15 20:42:22 +0530

    [Suresh] Add : python program to Reversing the frist and last name

---
+++ BasicPython-T2/fristNameLastNameReverse[1].py
@@ -0,0 +1,19 @@
+'''
+@Author: Suresh
+@Date: 2024-07-10 
+@Last Modified by: Suresh
+@Last Modified: 2024-07-11 
+@Title : Access Environment Variables
+'''
+
+def fristAndLast():
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    print(last_name + " " + first_name)
+
+def main():
+    fristAndLast()
+
+if __name__ == '__main__':
+    main()
+
