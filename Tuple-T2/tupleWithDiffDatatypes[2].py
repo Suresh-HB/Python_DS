d83a81318e1c530bd3c981bd6d590fe8756e7399
Suresh HB <sureshsai175475@gmail.com>
2024-07-15 20:28:20 +0530

    [Suresh] Add : Python program to creating the tuple and storing different data types

---
+++ Tuple-T2/tupleWithDiffDatatypes[2].py
@@ -0,0 +1,16 @@
+'''
+@Author: Suresh
+@Date: 2024-07-13
+@Last Modified by: Suresh
+@Last Modified: 2024-07-13 
+@Title : Python program to creating the tuple and storing different data types.
+'''
+
+
+def main():
+    mixed_tuple = ("apple", 42, 3.14, True)
+    print("Mixed Tuple:", mixed_tuple)
+
+
+if __name__ == '__main__':
+    main()
