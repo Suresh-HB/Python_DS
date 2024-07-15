1b29a43df28a219c05a3cbd4d439fafad12938d4
Suresh HB <sureshsai175475@gmail.com>
2024-07-15 20:31:55 +0530

    [Suresh] Add : Python program to convert list to tuple

---
+++ Tuple-T2/convertListToTuple[7].py
@@ -0,0 +1,17 @@
+'''
+@Author: Suresh
+@Date: 2024-07-13
+@Last Modified by: Suresh
+@Last Modified: 2024-07-13 
+@Title : Python program to convert list to tuple.
+'''
+
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    my_list = tuple(my_list)
+    print(my_list)
+
+
+if __name__ == '__main__':
+    main()
