4b602513a09294f24172e57f510d6251e6f89a51
Suresh HB <sureshsai175475@gmail.com>
2024-07-15 20:15:40 +0530

    [Suresh] Add : Python program to create a union of sets

---
+++ Sets-T2/creatingUnionSet[7].py
@@ -0,0 +1,27 @@
+'''
+@Author: Suresh
+@Date: 2024-07-12
+@Last Modified by: Suresh
+@Last Modified: 2024-07-12 
+@Title : Program to create a union of sets
+'''
+
+def union():
+    """
+    Description: Function to create uninon set
+    Parameter: 
+    Return: 
+    """
+
+
+    s1= {2, 4, 5, 6} 
+    s2= {4, 6, 7, 8}
+    print(s1.union(s2))
+
+
+def main():
+    union()
+
+
+if __name__ == 'main':
+    main()
