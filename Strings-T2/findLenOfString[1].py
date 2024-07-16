27d14fd1d9c74d542aa4126186c6519601110cc9
Suresh HB <sureshsai175475@gmail.com>
2024-07-16 17:39:51 +0530

    [Suresh] Add : Python program to find the length of string.

---
+++ Strings-T2/findLenOfString[1].py
@@ -0,0 +1,19 @@
+'''
+
+@Author: Suresh
+@Date: 2024-07-16 
+@Last Modified by: Suresh
+@Last Modified: 2024-07-16 
+@Title : Python program to find the length of string.
+
+'''
+
+
+def main():
+    st = "Python programming"
+    ln=len(st)
+    print(ln)
+
+
+if __name__ == '__main__':
+    main()
