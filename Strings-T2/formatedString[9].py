7924edc199272993ff357d5d481a5346706362c2
Suresh HB <sureshsai175475@gmail.com>
2024-07-16 17:40:21 +0530

    [Suresh] Add : Python program to display formatted text (width=50) as output.

---
+++ Strings-T2/formatedString[9].py
@@ -0,0 +1,20 @@
+'''
+
+@Author: Suresh
+@Date: 2024-07-16 
+@Last Modified by: Suresh
+@Last Modified: 2024-07-16 
+@Title : Python program to display formatted text (width=50) as output.
+
+'''
+
+
+def main():
+    
+    print("The table {} = {}".format('width', 50))
+
+
+if __name__ == '__main__':
+        main()
+
+
