e5e784ee561f04af1ca86fc3546ee4b09dc66df7
Suresh HB <sureshsai175475@gmail.com>
2024-07-23 20:21:23 +0530

    [Suresh] Add : Add logger to Python program for removing the first occurrence of an element from an array.

--- Python-DS_Array-T2/remove1stOccurrenceOfElement[4].py
+++ Python-DS_Array-T2/remove1stOccurrenceOfElement[4].py
@@ -6,6 +6,11 @@
 @Title : Removing the frist Occurrence of the element
 '''
 
+import os
+import loggingfile
+
+current_scriptName=os.path.basename(__file__)
+logger=loggingfile.logg(current_scriptName)
 
 def remove_first_occurrence(arr, element):
     """
@@ -14,17 +19,25 @@ def remove_first_occurrence(arr, element):
         arr, element : function takes one array and one element
     Return:
     """
-
+    logger.info("Inside remove_first_occurrence method")
 
     if element in arr:
         arr.remove(element)
+        logger.debug(f"{element} is removed")
+
 
 
 def main():
+
+    logger.info("Inside main method")
     arr = [1, 2, 3, 4, 2, 2, 3, 1, 1]
+    logger.debug(f"{arr} given array")
     element = 2
+    logger.debug(f"given element for remve occurrences {element} ")
 
     print("**Original array** :----> ", arr)
+
+    logger.info("Calling remove_first_occurrence method")
     remove_first_occurrence(arr, element)
     print("**Array after removed** :--->", element, ":", arr)
 
