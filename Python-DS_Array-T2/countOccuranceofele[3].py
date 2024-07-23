194358448c26249788804f1eb71902a99425dcb9
Suresh HB <sureshsai175475@gmail.com>
2024-07-23 20:17:02 +0530

    [Suresh] Add: Add logging to count occurrences of elements.

--- Python-DS_Array-T2/countOccuranceofele[3].py
+++ Python-DS_Array-T2/countOccuranceofele[3].py
@@ -8,7 +8,14 @@
 
 '''
 
+import loggingfile
+import os
 
+
+current_scriptname=os.path.basename(__file__)
+
+logger=loggingfile.logg(current_scriptname)
+              
 def count_occurrences(arr, element):
     """
     Description: Function to count the Occurrence of the incoming element
@@ -18,18 +25,24 @@ def count_occurrences(arr, element):
        count: returning the count of specified element 
     """
 
+    logger.info("inside count_occurrences method")
 
     count = 0
     for item in arr:
         if item == element:
             count += 1
+    logger.debug(f"{count} times the given element present and this method returning count")
     return count
 
 
 def main():
+
+    logger.info("Inside main method")
     
     arr = [1, 2, 3, 4, 2, 2, 3, 1, 1]
+    logger.debug(f"{arr} is the input")
     element = 2
+    logger.debug("calling count_occurrences method")
     print(f"Number of occurrences of {element} in the array: {count_occurrences(arr, element)} times")
 
 
